5cce366c7e770feae21ffcd5799e2c6ce7b55389
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-03-29 16:47:38 +0300

    Adım 1
    Project router en pointleri tanımlandı
    Nodemon eklendi

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,13 @@
 // "project" routerını buraya yazın!
+const express = require("express");
+
+const projectRouter = express();
+
+projectRouter.use(express.json());
+
+projectRouter.get("/", (req, res) => {});
+projectRouter.get("/:id", (req, res) => {});
+projectRouter.post("/", (req, res) => {});
+projectRouter.put("/:id", (req, res) => {});
+projectRouter.delete("/:id", (req, res) => {});
+projectRouter.get("/:id/actions", (req, res) => {});
